2b761f1867aff8651b72005bf413f3945588c66b
Abdul Majid <abdulmajidcse@gmail.com>
2024-01-17 12:20:30 +0600

    catch all route

---
+++ app/(frontend)/shop/[...categorySlug]/page.tsx
@@ -0,0 +1,14 @@
+export default function Shop({
+  params,
+}: {
+  params: { categorySlug: [string] };
+}) {
+  return (
+    <>
+      <h2>Hello, shop page!</h2>
+      <div className="font-bold text-purple-800">
+        {params.categorySlug.map((slug) => `${slug.toUpperCase()} -> `)}
+      </div>
+    </>
+  );
+}
